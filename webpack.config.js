19560694bd1edf0cf7dbcf3f85a9567d81b57561
Vishal <vpm911@gmail.com>
2018-11-18 16:22:39 +0530

    Production webpack build setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,52 @@
 const path = require('path');
-module.exports = {
-    mode:'development',
-    entry: './src/app.js',
-    output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-    }
-,
-module:{
-    rules:[{
-    loader: 'babel-loader',
-    test: /\.js$/,
-    exclude: /node_modules/
-    }, {
-    test: /\.s?css$/,
-    use : [
-        'style-loader',
-        'css-loader','sass-loader']
-    }
-]
-},
-    devtool:'cheap-eval-source-map',
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// This is to separate css into a different file and not include it in the bundle.js
 
-    devServer:{
-         contentBase: path.join(__dirname,'public'),
-         port:3400,
-         historyApiFallback:true
+module.exports = (env)=> {
+    console.log('env: '+env);
+    const isProduction = env==='production' ;
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+    return {
+        mode:'development',
+        entry: './src/app.js',
+        output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+        }
+    ,
+    module:{
+        rules:[{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+        }, {
+        test: /\.s?css$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+            }
+        ]
+        
+        }
+    ]
+    },
+        plugins:[CSSExtract],
+        devtool: isProduction ? 'source-map':'inline-source-map',
+        devServer:{
+            contentBase: path.join(__dirname,'public'),
+            port:3400,
+            historyApiFallback:true
+        }
     }
 };
